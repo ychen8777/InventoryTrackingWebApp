d0ca4b7020ab1d8896d1efc9a6d37d2bdf5ad720
ychen8777 <ychen8777@gmail.com>
2022-01-16 16:16:50 -0500

    import initializr files

---
+++ inventoryTrackingWebApp/src/main/java/com/example/inventoryTrackingWebApp/InventoryTrackingWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryTrackingWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackingWebAppApplication.class, args);
+	}
+
+}
---
+++ inventoryTrackingWebApp/src/test/java/com/example/inventoryTrackingWebApp/InventoryTrackingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryTrackingWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTrackingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
