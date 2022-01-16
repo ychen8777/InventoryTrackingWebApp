18b11de6220de8770fd6200b61a47b4cc437ea0e
ychen8777 <ychen8777@gmail.com>
2022-01-16 17:49:46 -0500

    Create ProductMapper.java

---
+++ inventoryTrackingWebApp/src/main/java/com/example/inventoryTrackingWebApp/Mapper/ProductMapper.java
@@ -0,0 +1,29 @@
+package com.example.inventoryTrackingWebApp.Mapper;
+
+import com.example.inventoryTrackingWebApp.model.Product;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface ProductMapper {
+
+    @Select("SELECT * FROM INVENTORY WHERE productId = #{productId}")
+    Product getProduct(Integer productId);
+
+    @Select("SELECT * FROM INVENTORY")
+    List<Product> getProductList();
+
+    @Insert("INSERT INTO INVENTORY (productId, productName, quantity) " +
+            "VALUES(#{productId}, #{productName}, #{quantity})")
+    @Options(useGeneratedKeys = true, keyProperty = "productId")
+    int insertProduct(Product product);
+
+    @Delete("DELETE FROM INVENTORY WHERE productId = #{productId}")
+    int deleteNote(Integer productId);
+
+    @Update("UPDATE INVENTORY SET productName = #{productName}, quantity = #{quantity}" +
+            "WHERE productId = #{productId}")
+    int updateNote(Product product);
+
+}
