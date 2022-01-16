7e3c5af58ecb907cb4103a59f49da205bf24022e
ychen8777 <ychen8777@gmail.com>
2022-01-16 17:31:41 -0500

    Create Product.java

---
+++ inventoryTrackingWebApp/src/main/java/com/example/inventoryTrackingWebApp/model/Product.java
@@ -0,0 +1,53 @@
+package com.example.inventoryTrackingWebApp.model;
+
+import java.util.Objects;
+
+public class Product {
+
+    private Integer productId;
+    private String productName;
+    private Integer quantity;
+
+    public Product(Integer productId, String productName, Integer quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId, product.productId) && Objects.equals(productName, product.productName) && Objects.equals(quantity, product.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, quantity);
+    }
+}
